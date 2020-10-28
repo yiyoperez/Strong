82fc717e4953f4385cd50015991f7e0aa7735d83
yiyoperez <yiyoperez99@gmail.com>
2020-10-28 02:59:10 -0300

    Implemented UpdateChecker, fixed explicit scoping :muscle:

--- Strong-Bukkit/src/main/java/com/strixmc/strong/bukkit/Strong.java
+++ Strong-Bukkit/src/main/java/com/strixmc/strong/bukkit/Strong.java
@@ -3,6 +3,7 @@ package com.strixmc.strong.bukkit;
 import com.strixmc.common.loader.Loader;
 import com.strixmc.strong.bukkit.utils.BinderModule;
 import com.strixmc.strong.bukkit.utils.ConfigUpdater;
+import com.strixmc.strong.bukkit.utils.UpdateChecker;
 import com.strixmc.strong.bukkit.utils.settings.Settings;
 import lombok.SneakyThrows;
 import me.yushust.inject.Injector;
@@ -16,13 +17,34 @@ import java.util.Arrays;
 
 public class Strong extends JavaPlugin {
 
-  @Inject @Named("LangLoader") Loader langLoader;
-  @Inject @Named("CommandsLoader") Loader commandsLoader;
+  @Inject @Named("LangLoader") private Loader langLoader;
+  @Inject @Named("CommandsLoader") private Loader commandsLoader;
   @Inject private Settings settings;
 
   @Override
   public void onEnable() {
     new MetricsLite(this, 9149);
+    UpdateChecker.init(this, 85026).requestUpdateCheck().whenComplete((result, exception) -> {
+      UpdateChecker.UpdateReason reason = result.getReason();
+      switch (reason) {
+        case NEW_UPDATE: {
+          this.getLogger().info(String.format("An update is available! Strong %s may be downloaded at %s", result.getNewestVersion(), "https://www.spigotmc.org/resources/85026/updates"));
+          break;
+        }
+        case UP_TO_DATE: {
+          this.getLogger().info(String.format("Your version of Strong (%s) is up to date!", result.getNewestVersion()));
+          break;
+        }
+        case COULD_NOT_CONNECT: {
+          this.getLogger().warning("Could not check for a new version of Strong. Reason: " + reason);
+          break;
+        }
+        case UNRELEASED_VERSION: {
+          this.getLogger().info(String.format("Your version of Strong (%s) is more recent than the one publicly available. Are you on a development build?", result.getNewestVersion()));
+          break;
+        }
+      }
+    });
 
     BinderModule binderModule = new BinderModule(this);
     Injector injector = binderModule.createInjector();
--- Strong-Proxy/src/main/java/com/strixmc/strong/proxy/Strong.java
+++ Strong-Proxy/src/main/java/com/strixmc/strong/proxy/Strong.java
@@ -3,6 +3,7 @@ package com.strixmc.strong.proxy;
 import com.strixmc.common.loader.Loader;
 import com.strixmc.strong.proxy.utils.BinderModule;
 import com.strixmc.strong.proxy.utils.FileManager;
+import com.strixmc.strong.proxy.utils.UpdateChecker;
 import com.strixmc.strong.proxy.utils.settings.Settings;
 import lombok.Getter;
 import me.yushust.inject.Injector;
@@ -22,6 +23,28 @@ public class Strong extends Plugin {
   @Override
   public void onEnable() {
     new MetricsLite(this, 9163);
+    UpdateChecker.init(this, 85040).requestUpdateCheck().whenComplete((result, exception) -> {
+      UpdateChecker.UpdateReason reason = result.getReason();
+      switch (reason) {
+        case NEW_UPDATE: {
+          this.getLogger().info(String.format("An update is available! Strong %s may be downloaded at %s", result.getNewestVersion(), "https://www.spigotmc.org/resources/85040/updates"));
+          break;
+        }
+        case UP_TO_DATE: {
+          this.getLogger().info(String.format("Your version of Strong (%s) is up to date!", result.getNewestVersion()));
+          break;
+        }
+        case COULD_NOT_CONNECT: {
+          this.getLogger().warning("Could not check for a new version of Strong. Reason: " + reason);
+          break;
+        }
+        case UNRELEASED_VERSION: {
+          this.getLogger().info(String.format("Your version of Strong (%s) is more recent than the one publicly available. Are you on a development build?", result.getNewestVersion()));
+          break;
+        }
+      }
+    });
+
 
     BinderModule binderModule = new BinderModule(this);
     Injector injector = binderModule.createInjector();
